1570e39f633cf72f7d120cda4477f8dd0f0fd30c
Luca Vitale <lucavitale@MBP-di-Luca-2.homenet.telecomitalia.it>
2026-04-29 20:13:40 +0200

    Fix: force port 8080

--- server/index.cjs
+++ server/index.cjs
@@ -6,7 +6,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3005;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
